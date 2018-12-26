2ef5292b22ea4295305fc439c4fba8c92b2e599d
Arun S. Sekher <arunshekher@users.noreply.github.com>
2018-12-27 02:04:03 +0400

    Implement admin pref decide which user event to use for Navatar creation.

--- e_module.php
+++ e_module.php
@@ -7,8 +7,20 @@ if ( ! defined('e107_INIT')) {
 }
 require_once __DIR__ . '/vendor/autoload.php';
 
-// signup activated event trigger
-e107::getEvent()->register('user_signup_activated', [UserEvents::class, 'confirmed']);
+$prefs = e107::getPlugPref('navatar');
 
-// user login event trigger
-e107::getEvent()->register('login', [UserEvents::class, 'login']);
+
+if($prefs['user_trigger_event'] === 3) {
+	e107::getEvent()->register('user_signup_activated', [UserEvents::class, 'confirmed']);
+	e107::getEvent()->register('login', [UserEvents::class, 'login']);
+}
+
+if ($prefs['user_trigger_event'] === 2) {
+	e107::getEvent()->register('user_signup_activated', [UserEvents::class, 'confirmed']);
+}
+
+if ($prefs['user_trigger_event'] === 1) {
+	e107::getEvent()->register('login', [UserEvents::class, 'login']);
+}
+
+//\Navatar\Plugin\Main::log($prefs, 'emodule-prefs');
