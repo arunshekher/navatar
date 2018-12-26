4423898a7bf1816c559f759dbbb76d94054c0d8e
Arun S. Sekher <arunshekher@users.noreply.github.com>
2018-12-26 17:43:53 +0400

    Add Navatar tidy-up wizard lans, add more project menu lans.

--- languages/English/English_admin.php
+++ languages/English/English_admin.php
@@ -7,8 +7,12 @@ define('LAN_NAVATAR_PREF_VAL_REALNAME', 'Real Name');
 define('LAN_NAVATAR_INFO_MENU_TITLE', 'Project Info');
 define('LAN_NAVATAR_INFO_MENU_SUBTITLE', 'Project on GitHub:');
 define('LAN_NAVATAR_INFO_MENU_SUBTITLE_ISSUES', 'Report Issues:');
-define('LAN_NAVATAR_INFO_MENU_SUBTITLE_DEV', 'Thank the Developer!');
-define('LAN_NAVATAR_INFO_MENU_SUPPORT_DEV_TEXT', 'If you enjoy this plugin, please consider supporting what I do.');
-define('LAN_NAVATAR_INFO_MENU_SUPPORT_DEV_TEXT_SIGN', '"Please don\'t blame me if it breaks something!" &mdash; Arun');
+define('LAN_NAVATAR_INFO_MENU_ISSUES_TEXT', 'Click the issues button below to report your issues on Github.');
+define('LAN_NAVATAR_INFO_MENU_SUBTITLE_DEV', 'Show your Appreciation!');
+define('LAN_NAVATAR_INFO_MENU_SUPPORT_DEV_TEXT', 'If you find the plugin useful, please consider supporting what I do.');
+define('LAN_NAVATAR_INFO_MENU_SUPPORT_DEV_TEXT_SIGN', 'In contrast, if it breaks something please don\'t blame me! &mdash; Arun');
+
+define('LAN_NAVATAR_TIDY_CONFIRM_ROLLBACK', 'Confirm rolling back the database entry for [count] user(s) created by NaVatar in \'user\' table.');
+define('LAN_NAVATAR_TIDY_USERS_LABEL', 'Navatar Users:');
 
 
