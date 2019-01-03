942bb962a5ae5898e018cf2846581230ec4207da
Arun S. Sekher <arunshekher@users.noreply.github.com>
2019-01-03 10:14:26 +0400

    Remove incorrectly inserted copyright annotation.

--- Plugin/Jobs/ProcessQueue.php
+++ Plugin/Jobs/ProcessQueue.php
@@ -1,18 +1,4 @@
 <?php
-/**
- * This file is part of e107dev.box distribution.
- * Copyright (c) 2016-2018, Arun S. Sekher.
- *
- * @category   Plugins
- * @package    Copyright
- * @version    1.0.0
- * @since      1.0.0
- * @author     Arun S. Sekher <arunshekher@hotmail.com>
- * @see        {@link http://github.com/shapeshed/copyright.ee_addon/}
- * @license    {@link http://www.gnu.org/licenses/}
- *
- */
-
 namespace Navatar\Plugin\Jobs;
 
 use Navatar\Plugin\Main;
